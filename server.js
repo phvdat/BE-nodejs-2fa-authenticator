a7f89fdd005ef1bcbb0a75e715e91c17ac608914
phamvdat <dat.pham@lfglobaltech.com>
2023-02-13 18:05:10 +0700

    fix cors and filed register

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
-// const cors = require('cors');
+var cors = require('cors')
 var bodyParser = require('body-parser')
 const express = require('express');
 const mongoose = require('mongoose');
+
+
+const app = express();
+// app.use(cors())
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(bodyParser.json());
 const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
@@ -15,15 +23,11 @@ database.on('error', (error) => {
 database.once('connected', () => {
 	console.log('Database Connected');
 })
-const app = express();
-// app.use(cors())
-app.use(express.json());
-app.use(bodyParser.json());
 
 const routes = require('./routes/routes');
 
 app.use('/api', routes)
 
-app.listen(5000, () => {
-	console.log(`Server Started at ${5000}`)
+app.listen(8000, () => {
+	console.log(`Server Started at ${8000}`)
 })
